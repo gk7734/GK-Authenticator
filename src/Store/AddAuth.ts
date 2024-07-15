f974997653cea0fe6629c8c082a109f146d68773
gk7734 <gamekiller7734@naver.com>
2024-07-15 09:44:37 +0900

    update to useAuthStore

--- src/Store/AddAuth.ts
+++ src/Store/AddAuth.ts
@@ -1,4 +1,6 @@
-import {create} from 'zustand';
+import {create, StateCreator} from 'zustand';
+import {createJSONStorage, persist, PersistOptions} from 'zustand/middleware';
+import AsyncStorage from '@react-native-async-storage/async-storage';
 
 // Auth 인터페이스 정의
 interface Auth {
@@ -14,17 +16,31 @@ interface AuthStore {
   removeAuth: (secret: string) => void;
 }
 
+// Persist 미들웨어 옵션 타입 정의
+type MyPersist = (
+  config: StateCreator<AuthStore>,
+  options: PersistOptions<AuthStore>,
+) => StateCreator<AuthStore>;
+
 // zustand 스토어 생성
-const useAuthStore = create<AuthStore>(set => ({
-  auths: [],
-  addAuth: (auth: Auth) =>
-    set(state => ({
-      auths: [...state.auths, auth],
-    })),
-  removeAuth: (secret: string) =>
-    set(state => ({
-      auths: state.auths.filter(auth => auth.secret !== secret),
-    })),
-}));
+const useAuthStore = create<AuthStore>(
+  (persist as MyPersist)(
+    set => ({
+      auths: [],
+      addAuth: auth =>
+        set(state => ({
+          auths: [...state.auths, auth],
+        })),
+      removeAuth: secret =>
+        set(state => ({
+          auths: state.auths.filter(auth => auth.secret !== secret),
+        })),
+    }),
+    {
+      name: 'auth-storage', // storage에 저장될 아이템의 이름
+      storage: createJSONStorage(() => AsyncStorage),
+    },
+  ),
+);
 
 export default useAuthStore;
